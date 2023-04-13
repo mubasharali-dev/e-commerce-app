1b20025a55828b25f849ec3bc089bb51946c9bf0
Mubashar Ali <mubashar.hashmat@gmail.com>
2023-04-13 05:10:34 +0500

    third commit

--- src/components/hero/Order.jsx
+++ src/components/hero/Order.jsx
@@ -5,22 +5,18 @@ const OrderProcess = () => {
   return (
     <div className="order-process">
       <div className="step">
-        <div className="number">1</div>
         <h3 className="number-1">Your Order</h3>
         <p>Choose the items you want to purchase</p>
       </div>
       <div className="step">
-        <div className="number">2</div>
         <h3 className="number-2">Ready Your Order</h3>
         <p>Confirm your order and make the payment</p>
       </div>
       <div className="step">
-        <div className="number">3</div>
         <h3 className="number-3">Packing your Order</h3>
         <p>We will pack your order carefully</p>
       </div>
       <div className="step">
-        <div className="number">4</div>
         <h3 className="number-4">And Deliver on time</h3>
         <p>Enjoy your order delivered on time</p>
       </div>
--- src/components/hero/Slider.jsx
+++ src/components/hero/Slider.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import image5 from "../../assets/images/image5.jpg";
 import image1 from "../../assets/images/image1.jpg";
 import image2 from "../../assets/images/image2.jpg";
-import image3 from "../../assets/images/image3.webp";
+import image4 from "../../assets/images/image4.jpg";
+import image6 from "../../assets/images/image6.jpg";
 import "./slider.css";
 const Slider = () => {
   return (
@@ -12,16 +12,16 @@ const Slider = () => {
       <div className="slider">
         <div className="container grid">
           <div className="img">
+            <img src={image4} alt="" />
+          </div>
+          <div className="img">
+            <img src={image6} alt="" />
+          </div>
+          <div className="img">
             <img src={image1} alt="" />
           </div>
           <div className="img">
             <img src={image2} alt="" />
-          </div>
-          <div className="img">
-            <img src={image3} alt="" />
-          </div>
-          <div className="img">
-            <img src={image5} alt="" />
           </div>
         </div>
       </div>
